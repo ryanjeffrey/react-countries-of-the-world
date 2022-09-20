afc248d3a362da91d10885c37159155baf48819b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-20 17:37:27 -0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/layout/Header/Header';
 import Main from './components/layout/Main/Main';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
     </div>
   );
