aa51ac0888de9ee0216dc4d17005ee48aa1ac964
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-20 14:57:53 -0500

    Display flag img by calling flagpedia API

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './CountryCard.css';
 
-export default function CountryCard({ name }) {
+export default function CountryCard({ name, iso2 }) {
   return (
-    <div className='country-card'>
-      <div className='country-name'>{name}</div>
+    <div className="country-card">
+      <div className="country-name">{name}</div>
+      <img src={`https://flagcdn.com/144x108/${iso2.toLowerCase()}.png`} height="64" />
     </div>
   );
 }
