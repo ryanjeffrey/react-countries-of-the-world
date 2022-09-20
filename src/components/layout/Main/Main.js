4dbb26fe64aef23d9a8ba0a04cbb417dc6baeac7
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-20 17:19:51 -0500

    Apply custom hook with dropdown menu

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -5,12 +5,29 @@ import CountryCard from '../../CountryCard/CountryCard';
 import './Main.css';
 
 export default function Main() {
-  const countries = useCountries();
+  const { filterCountries, continent, setContinent } = useCountries();
   return (
-    <main>
-      {countries.map((country) => (
-        <CountryCard key={country.id} {...country} />
-      ))}
-    </main>
+    <>
+      <select
+        value={continent}
+        onChange={(e) => {
+          setContinent(e.target.value);
+        }}
+      >
+        <option value="all">All Countries</option>
+        <option value="Africa">Africa</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+        <option value="North America">North America</option>
+        <option value="South America">South America</option>
+      </select>
+      <main>
+        {filterCountries().map((country) => (
+          <CountryCard key={country.id} {...country} />
+        ))}
+      </main>
+    </>
   );
 }
