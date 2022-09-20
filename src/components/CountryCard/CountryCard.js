04e568c9cb6cb7f345c4035281ce471edddf2e45
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-20 16:43:24 -0500

    Add alt tag to flag imgs

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -5,7 +5,7 @@ export default function CountryCard({ name, iso2 }) {
   return (
     <div className="country-card">
       <div className="country-name">{name}</div>
-      <img src={`https://flagcdn.com/144x108/${iso2.toLowerCase()}.png`} height="64" />
+      <img src={`https://flagcdn.com/144x108/${iso2.toLowerCase()}.png`} height="64" alt={name} />
     </div>
   );
 }
