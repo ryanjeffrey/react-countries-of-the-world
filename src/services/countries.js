0ffacf8412446c56d4ba7824145cef956af483b9
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-20 19:24:44 -0500

    Sort countries by name

--- src/services/countries.js
+++ src/services/countries.js
@@ -1,6 +1,6 @@
 import { checkError, client } from './client';
 
 export async function fetchCountries() {
-  const response = await client.from('countries').select('*');
+  const response = await client.from('countries').select('*').order('name');
   return checkError(response);
 }
